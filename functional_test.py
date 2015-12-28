f4d9665cdf389c1089f78c806cf3ce47b08f16f0
yooyoung-mo <gigamadness@gmail.com>
2015-12-29 05:07:40 +0900

    기능 테스트가 작업 아이템을 추가할수 있는지 확인

--- functional_test.py
+++ functional_test.py
@@ -2,6 +2,7 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 __author__ = 'yooyoung-mo'
 
@@ -23,15 +24,24 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
 
         # 그녀는 바로 작업을 추가하기로 한다
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), '작업 아이템 입력')
 
-        # "공작 깃털 사기"라고 텍스트 상자에 입력 한다.
+        # "공작 깃털 사기"라고 텍스트 상자에 입력 한다
+        inputbox.send_keys('공작 깃털 사기')
 
         # 엔터 키를 치면 페이지가 갱신되고 작업 목록에
         # "1: 공작깃털 사기 아이템이 추가 된다.
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: 공작 깃털 사기' for row in rows)
+        )
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다.
         # 다시 "공작 깃털을 이용하여 그물 만들기" 라고 입력 한다.
-
         # 페이지가 다시 갱신 되고, 두 개 아이템이 목록에 보인다.
         # 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다
         # 사이트는 그녀를 위한 특정 URL을 생성해 준다
