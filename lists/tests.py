394d6458d13e3bd9048c8aca7b6ab09961c30c82
yooyoung-mo <gigamadness@gmail.com>
2015-12-30 05:37:32 +0900

    아이템용 모델과 마이그레이션 연계

--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
 
+from lists.models import Item
 from lists.views import home_page
 
 
@@ -35,3 +36,23 @@ class HomePageTest(TestCase):
 
         expected_html = render_to_string('home.html', {'new_item_text': u'신규 작업 아이템'})
         self.assertEqual(response.content.decode('utf-8'), expected_html)
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = u'첫 번째 아이템'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = u'두 번째 아이템'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, u'첫 번째 아이템')
+        self.assertEqual(second_saved_item.text, u'두 번째 아이템')
