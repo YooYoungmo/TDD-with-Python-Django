ca76008413d65d80e56927f461d35a5115447f6b
yooyoung-mo <gigamadness@gmail.com>
2015-12-30 04:58:03 +0900

    아이템 확인 assert 리펙토링

--- functional_test.py
+++ functional_test.py
@@ -34,9 +34,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: 공작깃털 사기 아이템이 추가 된다.
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(u'1: 공작 깃털 사기', [row.text for row in rows])
+        self.check_for_row_in_list_table(u'1: 공작 깃털 사기')
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다.
         # 다시 "공작 깃털을 이용하여 그물 만들기" 라고 입력 한다.
@@ -45,11 +43,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # 페이지가 다시 갱신 되고, 두 개 아이템이 목록에 보인다.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(u'1: 공작 깃털 사기', [row.text for row in rows])
-        self.assertIn(u'2: 공작 깃털을 이용하여 그물 만들기', [row.text for row in rows])
-
+        self.check_for_row_in_list_table(u'1: 공작 깃털 사기')
+        self.check_for_row_in_list_table(u'2: 공작 깃털을 이용하여 그물 만들기')
 
         # 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다
         # 사이트는 그녀를 위한 특정 URL을 생성해 준다
@@ -58,3 +53,8 @@ class NewVisitorTest(unittest.TestCase):
         # 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다
 
         # 만족하고 잠자리에 든다
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
