a1d4730c1c4b7c8fcc871f00e84e198ddab51e46
yooyoung-mo <gigamadness@gmail.com>
2016-01-06 05:53:49 +0900

    레이아웃과 스타일링을 위한 FT 첫 단계

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -86,3 +86,20 @@ class NewVisitorTest(LiveServerTestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
+
+    def test_layout_and_styling(self):
+        # 에디스는 메인 페이지를 방문 한다
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # 그녀는 입력 상자가 가운데 배치된 것을 본다
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        time.sleep(2)
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
+
+        # 그녀는 새로운 리스트를 시작하고 입력 상자가
+        # 가운데 배치된 것을 확인 한다
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        time.sleep(2)
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
