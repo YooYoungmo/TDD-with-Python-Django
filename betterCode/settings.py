82f372ef1c05a08c889d2ec12eda4b9f935bd9af
yooyoung-mo <gigamadness@gmail.com>
2016-01-09 11:54:44 +0900

    STATIC_ROOT 설정과 관리자 사이트 무효화

--- betterCode/settings.py
+++ betterCode/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -81,7 +81,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/lists/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
 
