d43f073d514b33e39cf2cd0401dbb4519fcd81c6
yooyoung-mo <gigamadness@gmail.com>
2016-01-09 11:31:56 +0900

    부트스트랩 추가 스타일링

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
-import time
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 __author__ = 'yooyoung-mo'
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
