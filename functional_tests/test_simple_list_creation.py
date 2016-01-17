ddcc243620f79c213193471793c46005d4d893c3
yooyoung-mo <gigamadness@gmail.com>
2016-01-17 22:18:08 +0900

    FT를 개별 파일로 분할

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,34 +1,14 @@
 # -*- coding: utf-8 -*-
-import sys
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
+from functional_tests.base import FunctionalTest
 
 __author__ = 'yooyoung-mo'
 
 
-class NewVisitorTest(StaticLiveServerTestCase):
-    @classmethod
-    def setUpClass(cls):
-        for arg in sys.argv:
-            if 'liveserver' in arg:
-                cls.server_url = 'http://' + arg.split('=')[1]
-                return
-        super(NewVisitorTest, cls).setUpClass()
-        cls.server_url = cls.live_server_url
-
-    @classmethod
-    def tearDownClass(cls):
-        if cls.server_url == cls.live_server_url:
-            super(NewVisitorTest, cls).tearDownClass()
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
-
-    def tearDown(self):
-        self.browser.quit()
-
+class NewVisitorTest(FunctionalTest):
     def test_can_start_a_list_retrieve_it_later(self):
         # 에디스는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
         # 해당 웹 사이트를 확인하러 간다
@@ -94,22 +74,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # 둘다 만족하고 잠자리에 든다
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
-    def test_layout_and_styling(self):
-        # 에디스는 메인 페이지를 방문 한다
-        self.browser.get(self.server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # 그녀는 입력 상자가 가운데 배치된 것을 본다
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
-
-        # 그녀는 새로운 리스트를 시작하고 입력 상자가
-        # 가운데 배치된 것을 확인 한다
-        inputbox.send_keys('testing\n')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
