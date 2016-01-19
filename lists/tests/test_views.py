7e936556fc03fd87c5153a4e91721efb88a7f4c1
yooyoung-mo <gigamadness@gmail.com>
2016-01-19 21:48:16 +0900

    list view를 리펙토링해서 신규 아이템 POST를 처리하도록 함

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -86,12 +86,10 @@ class ListViewTest(TestCase):
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
-
-class NewItemTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
         correct_list = List.objects.create()
 
-        self.client.post('/lists/%d/add_item' % (correct_list.id,), data={'item_text': u'기존 목록에 신규 아이템'})
+        self.client.post('/lists/%d/' % (correct_list.id,), data={'item_text': u'기존 목록에 신규 아이템'})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
@@ -101,6 +99,6 @@ class NewItemTest(TestCase):
     def test_redirects_to_list_view(self):
         correct_list = List.objects.create()
 
-        response = self.client.post('/lists/%d/add_item' % (correct_list.id,), data={'item_text': u'기존 목록에 신규 아이템'})
+        response = self.client.post('/lists/%d/' % (correct_list.id,), data={'item_text': u'기존 목록에 신규 아이템'})
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
