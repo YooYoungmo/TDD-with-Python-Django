10a729329ba624a33a66cc2f8f65cf19a33cdb70
yooyoung-mo <gigamadness@gmail.com>
2016-01-24 08:24:32 +0900

    모델 계층의 중복 아이템 검증 구현

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
 class List(models.Model):
@@ -7,8 +8,15 @@ class List(models.Model):
         return reverse('view_list', args=[self.id])
 
 
+@python_2_unicode_compatible
 class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        unique_together = ('list', 'text')
 
 
