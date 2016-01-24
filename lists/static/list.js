5b6a4fbbfe17202b54f9798a4d3fe423ba89d9e0
yooyoung-mo <gigamadness@gmail.com>
2016-01-24 14:44:28 +0900

    jQuery document on ready 적용

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,6 +1,8 @@
 /**
  * Created by yooyoung-mo on 2016. 1. 24..
  */
-$('input').on('keypress', function(){
-    $('.has-error').hide()
+$(document).ready(function($){
+    $('input').on('keypress', function(){
+        $('.has-error').hide()
+    });
 });
