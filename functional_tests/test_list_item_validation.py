dff38cef32bd321a56e60f20bb883ff6c56eafbc
yooyoung-mo <gigamadness@gmail.com>
2016-01-24 14:08:54 +0900

    에러 메시지 사라지기 실패하는 테스트 케이스 작성

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -14,7 +14,7 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('\n')
 
         # 페이지가 새로고침되고, 빈 아이템을 등록할 수 없다는 에러 메시지가 표시된다
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, EMPTY_LIST_ERROR)
 
         # 다른 아이템을 입력하고 이번에는 정상 처리된다
@@ -26,7 +26,7 @@ class ItemValidationTest(FunctionalTest):
 
         # 리스트 페이지에 다시 에러 메시지가 표시 된다
         self.check_for_row_in_list_table(u'1: 우유 사기')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, EMPTY_LIST_ERROR)
 
         # 아이템을 입력하면 정상 동작 한다
@@ -45,6 +45,23 @@ class ItemValidationTest(FunctionalTest):
 
         # 도움이 되는 에러 메시지를 본다
         self.check_for_row_in_list_table(u'1: 콜라 사기')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, DUPLICATE_ITEM_ERROR)
 
+    def test_error_message_are_cleared_on_input(self):
+        # 에디스는 검증 에러를 발생 시키도록 신규 목록을 시작한다
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        # 에러를 제거하기 위해 입력 상자에 타이핑을 시작한다
+        self.get_item_input_box().send_keys('a')
+
+        # 에러 메시지가 사라진 것을 보고 기뻐한다
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
+
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
