afca7bf5f8ea1892ba5762f08f97e14080e3dba2
yooyoung-mo <gigamadness@gmail.com>
2016-02-21 08:33:04 +0900

    chapter 15 끝

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -2,10 +2,28 @@
  * Created by yooyoung-mo on 2016. 2. 9..
  */
 
-var initialize = function (navigator) {
+/*global
+    $
+*/
+var initialize = function (navigator, user, token, urls) {
     "use strict";
     $('#id_login').on('click', function () {
         navigator.id.request();
+    });
+
+    navigator.id.watch({
+        loggedInUser: user,
+        onlogin: function (assertion) {
+            $.post(
+                urls.login,
+                {assertion: assertion, csrfmiddlewaretoken: token}
+            ).fail(function () {
+                navigator.id.logout();
+            }).done(function () {
+                window.location.reload();
+            });
+        },
+        onlogout: function () {}
     });
 };
 
