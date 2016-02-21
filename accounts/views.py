c33e48189da6e3d8b502425c6bd9e1da4382dccb
yooyoung-mo <gigamadness@gmail.com>
2016-02-21 09:25:46 +0900

    accounts.view Mocking

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth import authenticate
 
 
 def persona_login(request):
-    authenticate(assertion=request.POST['assertion'])
-    return HttpResponse()
+    user = authenticate(assertion=request.POST['assertion'])
+    if user:
+        login(request, user)
+    return HttpResponse('OK')
