11977d11dfbd58135c7b7125bd55cb332ed58a43
yooyoung-mo <gigamadness@gmail.com>
2016-03-05 10:12:54 +0900

    로그 추가

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import logging
+
 from django.contrib.auth import get_user_model
 
 __author__ = 'yooyoung-mo'
@@ -15,9 +17,12 @@ class PersonaAuthenticationBackend:
         pass
 
     def authenticate(self, assertion):
+        logging.warn('authenticate 함수 진입')
         response = requests.post(
                 PERSONA_VERIFY_URL,
                 data={'assertion': assertion, 'audience': DOMAIN})
+        logging.warn('퍼소나에서 응답 받음')
+        logging.warn(response.content.decode())
         if response.ok and response.json()['status'] == 'okay':
             email = response.json()['email']
             try:
